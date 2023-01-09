693138b3a2bcbc918e92aa4d7e3278f1ce32c879
Karthi-7 <karthikeyancvl96@gmail.com>
2023-01-10 00:01:01 +0530

    get req

--- src/pages/bookingpage/Bookingpage.js
+++ src/pages/bookingpage/Bookingpage.js
@@ -28,7 +28,7 @@ function Bookingpage() {
         try{
             setloading(true)
           
-            const response=(await axios.post('https://orbiz-rooms-client.onrender.com/orbizRooms/getroomById',{roomId:roomId,checkin:checkin,checkout:checkout})).data
+            const response=(await axios.get('https://orbiz-rooms-client.onrender.com/orbizRooms/getroomById',{roomId:roomId,checkin:checkin,checkout:checkout})).data
             console.log("res",response)
             setroom(response)
             setloading(false)
